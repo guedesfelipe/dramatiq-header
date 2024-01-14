8bd38803aba79a660d6f3339999ba3546bce0a8c
Felipe Guedes <contatofelipeguedes@gmail.com>
2024-01-14 02:08:06 -0300

    ✨ Add lib code

---
+++ dramatiq_header/__init__.py
@@ -0,0 +1,34 @@
+from dramatiq.middleware import Middleware
+import contextvars
+
+
+
+class HeadersMessage(Middleware):
+    """Middleware that exposes header via options message via a thread-local
+    variable.
+
+    """
+
+    _HEADERS: contextvars.ContextVar[
+        dict
+    ] = contextvars.ContextVar("_HEADERS", default={})
+
+    def __init__(self, header_name: str | None = None) -> None:
+        self.header_name = header_name
+
+    @classmethod
+    def get_headers(cls):
+        """Get the message that triggered the current actor.  Messages
+        are thread local so this returns ``None`` when called outside
+        of actor code.
+        """
+        return cls._HEADERS.get()
+
+    def before_process_message(self, _, message):
+        if not self.header_name:
+            self._HEADERS.set(message.options)
+        else:
+            # TODO: Add DEBUG log when don't find the header
+            self._HEADERS.set(
+                message.options.get(self.header_name, message.options)
+            )
